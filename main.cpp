99edc65ffccee17c1704dba93924e5907de22a18
Georgia Mason <140232973+georgiapoppy@users.noreply.github.com>
2025-01-06 10:10:47 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,41 +5,53 @@ int main(void) {
     DocumentManager documentmanager;
 
     std::string input;
-    std::getline(std::cin, input);
-    std::istringstream iss(input);
-    std::vector<std::string> functions;
-    std::string function;
-    while (iss >> function) {
-        functions.push_back(function);
-    }
-    
-    for (int i=0; i<functions.size()-1; i++) {
-        if (functions[i][0] == 'P') {
-            int id = stoi(functions[i].substr(1));
+    while (input != "STOP") {
+        std::cout << "ENTER INSTRUCTION: ";
+        std::cin >> input;
+        if (input == 'AU') {
+            int id;
+            std::cout << "Enter User ID: ";
+            std::cin >> id;
             documentmanager.addPatron(id);
             std::cout << "Patron " << id << " added successfully." << std::endl;
-        if (functions[i][0] == 'D') {
-            int name = stoi(functions[i].substr(1));
-            int id = stoi(functions[i].substr(2));
-            int license = stoi(functions[i].substr(3));
+        if (input == 'AB') {
+            std::string name;
+            std::cout << "Enter Book Name: ";
+            std::cin >> name;
+            int id;
+            std::cout << "Enter Book ID: ";
+            std::cin >> id;
+            int license;
+            std::cout << "Enter Liscense Limit: ";
+            std::cin >> license;
             documentmanager.addDocument(name, id, license);
             std::cout << "Document " << name << " added successfully." << std::endl;
-        if (functions[i][0] == 'S') {
-            int name = stoi(functions[i].substr(1));
+        if (input == 'S') {
+            std::string name;
+            std::cout << "Enter Book Name: ";
+            std::cin >> name;
             int id = documentmanager.search(name);
             std::cout << "Document " << name << " has ID " << id << "." << std::endl;
-        if (functions[i][0] == 'B') {
-            int id = stoi(functions[i].substr(1));
-            int id2 = stoi(functions[i].substr(2));
+        if (input == 'B') {
+            int id;
+            std::cout << "Enter Book ID: ";
+            std::cin >> id;
+            int id2;
+            std::cout << "Enter User ID: ";
+            std::cin >> id2;
             bool outcome = documentmanager.borrowDocument(id, id2);
             if (outcome == true) {
                 std::cout << "Document " << id << " borrowed successfully by patron " << id2 << "." << std::endl;
             } else {
                 std::cout << "Unsuccessful. Make sure the Document ID and Patron ID is correct." << std::endl;
             }
-        } else if (functions[i][0] == 'R') {
-            int id = stoi(functions[i].substr(1));
-            int id2 = stoi(functions[i].substr(2));
+        } else if (input == 'R') {
+            int id;
+            std::cout << "Enter Book ID: ";
+            std::cin >> id;
+            int id2;
+            std::cout << "Enter User ID: ";
+            std::cin >> id2;
             documentmanager.returnDocument(id, id2);
             std::cout << "Document " << id << " returned by patron " << id2 << "." << std::endl;
         }
