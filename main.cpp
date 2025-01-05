b629e12131e125b0ff0214b51534039698bcd1cd
Georgia Mason <140232973+georgiapoppy@users.noreply.github.com>
2025-01-06 08:52:43 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,6 @@
 
 int main(void) {
     DocumentManager documentmanager;
-    
-    documentmanager.addPatron(1);
-    documentmanager.addPatron(2);
-    documentmanager.addDocument("hi", 1, 2);
-    documentmanager.addDocument("hello", 2, 1);
-
-    std::cout << documentmanager.borrowDocument(2, 1) << std::endl;
-    std::cout << documentmanager.borrowDocument(2, 2) << std::endl;
-
-    documentmanager.returnDocument(2, 2);
-    documentmanager.returnDocument(2, 2); 
 
     std::string input;
     std::getline(std::cin, input);
@@ -27,22 +16,22 @@ int main(void) {
     for (int i=0; i<functions.size()-1; i++) {
         if (functions[i][0] == 'P') {
             int id = stoi(functions[i].substr(1));
-            addPatron(id);
+            documentmanager.addPatron(id);
             std::cout << "Patron " << id << " added successfully." << std::endl;
         if (functions[i][0] == 'D') {
             int name = stoi(functions[i].substr(1));
             int id = stoi(functions[i].substr(2));
             int license = stoi(functions[i].substr(3));
-            addDocument(name, id, license);
+            documentmanager.addDocument(name, id, license);
             std::cout << "Document " << name << " added successfully." << std::endl;
         if (functions[i][0] == 'S') {
             int name = stoi(functions[i].substr(1));
-            int id = search(name);
+            int id = documentmanager.search(name);
             std::cout << "Document " << name << " has ID " << id << "." << std::endl;
         if (functions[i][0] == 'B') {
             int id = stoi(functions[i].substr(1));
             int id2 = stoi(functions[i].substr(2));
-            bool outcome = borrowDocument(id, id2);
+            bool outcome = documentmanager.borrowDocument(id, id2);
             if (outcome == true) {
                 std::cout << "Document " << id << " borrowed successfully by patron " << id2 << "." << std::endl;
             } else {
@@ -51,7 +40,7 @@ int main(void) {
         } else if (functions[i][0] == 'R') {
             int id = stoi(functions[i].substr(1));
             int id2 = stoi(functions[i].substr(2));
-            returnDocument(id, id2);
+            documentmanager.returnDocument(id, id2);
             std::cout << "Document " << id << " returned by patron " << id2 << "." << std::endl;
         }
     }
